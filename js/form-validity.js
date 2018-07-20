39f581d098735731e1561b0a4d65d12fc2a79292
IerominSergii <iconboooook@gmail.com>
2018-07-20 14:44:58 +0300

    21 Учебный проект: pimp my mage

--- js/form-validity.js
+++ js/form-validity.js
@@ -1,42 +1,24 @@
 'use strict';
 
 (function () {
-  // constants
-  var ENTER_KEYCODE = 13;
-
-  // elements
   var userDialog = document.querySelector('.setup');
-  var setupWizardForm = userDialog.querySelector('.setup-wizard-form');
-  var setupSubmit = userDialog.querySelector('.setup-submit');
   var userNameInput = userDialog.querySelector('.setup-user-name');
 
-  // functions
-  var submitSetupWizardForm = function () {
-    if (setupWizardForm.valiidity.valid === true) {
-      setupWizardForm.submit();
-    }
-  };
-
-  // export
   window.formValidity = {
-    setupSubmitEnterPressHandler: function (evt) {
-      if (document.activeElement === setupSubmit && evt.keyCode === ENTER_KEYCODE) {
-        submitSetupWizardForm();
-      }
-    },
-    setupSubmitClickHandler: function () {
-      submitSetupWizardForm();
-    },
     userNameInputChangeHandler: function () {
       if (userNameInput.validity.tooShort) {
-        userNameInput.setCustomValidity('Имя персонажа не может содержать менее 2 символов');
+        userNameInput.setCustomValidity(
+            'Имя персонажа не может содержать менее 2 символов'
+        );
       } else if (userNameInput.validity.tooLong) {
-        userNameInput.setCustomValidity('Максимальная длина имени персонажа — 25 символов');
+        userNameInput.setCustomValidity(
+            'Максимальная длина имени персонажа — 25 символов'
+        );
       } else if (userNameInput.validity.valueMissing) {
         userNameInput.setCustomValidity('Обязательное поле');
       } else {
         userNameInput.setCustomValidity('');
       }
-    },
+    }
   };
 })();
--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@
   var ESC_KEYCODE = 27;
   var SETUP_INITIAL_POSITION = {
     x: 622,
-    y: 80,
+    y: 80
   };
 
   // global
@@ -14,25 +14,29 @@
   var hideElement = window.util.hideElement;
   var getRandomElement = window.util.getRandomElement;
   var constants = window.constants;
-  var setupSubmitEnterPressHandler = window.setupSubmitEnterPressHandler;
-  var setupSubmitClickHandler = window.setupSubmitClickHandler;
-  var userNameInputChangeHandler = window.userNameInputChangeHandler;
+  var userNameInputChangeHandler =
+    window.formValidity.userNameInputChangeHandler;
+  var renderWizard = window.renderWizard;
+  var saveForm = window.backend.save;
+  var load = window.backend.load;
 
   // elements
   var userDialog = document.querySelector('.setup');
+  var setupWizardForm = userDialog.querySelector('.setup-wizard-form');
+  var setupSubmit = userDialog.querySelector('.setup-submit');
   var setupClose = userDialog.querySelector('.setup-close');
   var userNameInput = userDialog.querySelector('.setup-user-name');
-  var setupSubmit = userDialog.querySelector('.setup-submit');
   var setupPlayer = userDialog.querySelector('.setup-player');
   var wizardCoat = userDialog.querySelector('.wizard-coat');
   var wizardEyes = userDialog.querySelector('.wizard-eyes');
+  var setupSimilar = userDialog.querySelector('.setup-similar');
+  var similarListElement = setupSimilar.querySelector('.setup-similar-list');
   var setupFireballWrap = setupPlayer.querySelector('.setup-fireball-wrap');
   var setupFireballInput = setupFireballWrap.querySelector('input');
   var setupFireball = setupFireballWrap.querySelector('.setup-fireball');
 
   var setupOpen = document.querySelector('.setup-open');
   var setupOpenIcon = setupOpen.querySelector('.setup-open-icon');
-
 
   // functions
   var setSetupInitialPosition = function () {
@@ -48,6 +52,35 @@
     element.style.backgroundColor = colors[getRandomElement(colors.length)];
   };
 
+  var submitSetupWizardForm = function () {
+    if (userNameInput.valiidity.valid === true) {
+      saveForm(new FormData(setupWizardForm), closeSetup);
+    }
+  };
+
+  var successHandler = function (dataArray) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(dataArray[i]));
+    }
+    similarListElement.appendChild(fragment);
+    setupSimilar.classList.remove('hidden');
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style =
+      'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
   var openSetup = function () {
     setupClose.addEventListener('click', closeSetup);
     document.addEventListener('keydown', userDialogEscPressHandler);
@@ -57,6 +90,7 @@
 
     userNameInput.addEventListener('change', userNameInputChangeHandler);
     setupPlayer.addEventListener('click', setupPlayerClickHandler);
+    load(successHandler, errorHandler);
     showElement(userDialog);
   };
 
@@ -93,18 +127,38 @@
   };
 
   var userDialogEscPressHandler = function (evt) {
-    if (document.activeElement !== userNameInput && evt.keyCode === ESC_KEYCODE) {
+    if (
+      document.activeElement !== userNameInput &&
+      evt.keyCode === ESC_KEYCODE
+    ) {
       evt.preventDefault();
       closeSetup();
     }
   };
 
   var setupCloseEnterPressHandler = function (evt) {
-    if (document.activeElement === setupClose && evt.keyCode === ENTER_KEYCODE) {
+    if (
+      document.activeElement === setupClose &&
+      evt.keyCode === ENTER_KEYCODE
+    ) {
       closeSetup();
     }
   };
 
+  var setupSubmitEnterPressHandler = function (evt) {
+    if (
+      document.activeElement === setupSubmit &&
+      evt.keyCode === ENTER_KEYCODE
+    ) {
+      submitSetupWizardForm();
+      evt.preventDefault();
+    }
+  };
+
+  var setupSubmitClickHandler = function (evt) {
+    submitSetupWizardForm();
+    evt.preventDefault();
+  };
 
   setupOpen.addEventListener('click', openSetup);
   setupOpenIcon.addEventListener('keydown', setupOpenIconEnterPressHandler);
